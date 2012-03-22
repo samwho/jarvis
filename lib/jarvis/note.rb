08b18f90a1bac5409f4c3a212166cd2d815a3b77
Sam Rose <samwho@lbak.co.uk>
2012-03-22 14:58:40 +0000

    Added documentation and constants to the Note class.

--- lib/jarvis/note.rb
+++ lib/jarvis/note.rb
@@ -2,11 +2,31 @@ class Note
   include MIDIator::Notes
   attr_accessor :note, :duration
 
-  def initialize note = C2, duration = 1
+  # Note length constants for readability
+  WHOLE        = 0
+  HALF         = 1
+  QUARTER      = 2
+  EIGHTH       = 3
+  SIXTEENTH    = 4
+  THIRTYSECOND = 5
+
+  def initialize note = C2, duration = QUARTER
     @note     = note
     @duration = duration
   end
 
+  # Gets a note length based on the following formula:
+  #   1 / 2 ^ note_length
+  #
+  # Where note_length is the value passed into this method. There are various
+  # constants on this class that help with this method, they are as follows:
+  #
+  # Note::WHOLE
+  # Note::HALF
+  # Note::QUARTER
+  # Note::EIGHTH
+  # Note::SIXTEENTH
+  # Note::THIRTYSECOND
   def self.length note_length
     1.0/2 ** note_length
   end
