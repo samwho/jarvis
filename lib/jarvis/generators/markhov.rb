0b57feb88fbf7f8ae06d08feefa159cd997d225f
Sam Rose <samwho@lbak.co.uk>
2012-03-28 01:10:57 +0100

    Added lookahead to the Markhov Chain generator. Didn't sound as good as I had hoped. I've also realised that I should probably separate the channels in the learnsong executable. It seems to be polluting the output.

--- lib/jarvis/generators/markhov.rb
+++ lib/jarvis/generators/markhov.rb
@@ -4,29 +4,19 @@ module Jarvis::Generators
   class MarkhovChains < NoteGenerator
     def initialize path = 'data/db2.ndb', lookahead = 0
       @data      = JSON.parse File.open(path) { |file| file.read }
-      @lookahead = 0
+      @lookahead = lookahead
+      @prev_notes = []
     end
 
     def next
-      # Get a random number between 0 and the sum of all counts in the note
-      # database file specified to the constructor.
-      r = rand sum_counts
-      d = @data.each
-
-      # Loop through the JSON nodes and subtract the count in that node from the
-      # random number generated above. If the random number falls below 0,
-      # return this node.
-      #
-      # This is basically just a method of selecting a random node from the note
-      # database file.
-      node = loop do
-        n  = d.next
-        r -= n[1]['count']
-
-        if r <= 0
-          break n
+      data = @data
+      @lookahead.times do |i|
+        if @prev_notes.length > i and data.keys.include? @prev_notes[i]
+          data = data[@prev_notes[i]]["next"]
         end
       end
+
+      node = select_random_node data
 
       # Get a random number between 0 and the sum of all of the durations in the
       # random node selected above.
@@ -47,6 +37,11 @@ module Jarvis::Generators
         end
       end
 
+      # Add the last note to top of the previous notes and pop the oldest note
+      # off if our array is greater than the lookahead.
+      @prev_notes.unshift(node[0])
+      @prev_notes.pop if @prev_notes.length > @lookahead
+
       # Because durations are not specified in midi time pulses in the note
       # database file, we need to divide the number by something. 10 sound good
       # to me.
@@ -55,6 +50,35 @@ module Jarvis::Generators
 
     def handle_input input
 
+    end
+
+    # Selects a random node from a Hash based on the contents of the 'counts'
+    # member on the has. Don't use this on any old hash, it need to be a hash in
+    # the same format as what is created by the learnsong executable.
+    #
+    # The return value will be an array of two parts:
+    #
+    # [node_key, node_value]
+    def select_random_node nodes
+      # Get a random number between 0 and the sum of all counts in the note
+      # database file specified to the constructor.
+      r = rand sum_counts(nodes)
+      d = nodes.each
+
+      # Loop through the JSON nodes and subtract the count in that node from the
+      # random number generated above. If the random number falls below 0,
+      # return this node.
+      #
+      # This is basically just a method of selecting a random node from the note
+      # database file.
+      return loop do
+        n  = d.next
+        r -= n[1]['count']
+
+        if r <= 0
+          break n
+        end
+      end
     end
 
     # Because chords are represented as string of comma separated integers in
@@ -67,10 +91,10 @@ module Jarvis::Generators
     # Returns the sum of the 'count' elements in each data entry in the
     # @data hash. Essentially this is returning the total number of notes
     # in all of the scanned songs in the given note database.
-    def sum_counts
+    def sum_counts data = @data
       sum = 0
 
-      @data.each do |key, value|
+      data.each do |key, value|
         sum += value['count']
       end
 
