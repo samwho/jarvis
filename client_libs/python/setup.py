a25000a7b9180ccfc047971dc3ffd3f55dc5c5c6
Sam Rose <samwho@lbak.co.uk>
2012-03-30 18:08:13 +0100

    Added a license to this project and started on the Python client library.

---
+++ client_libs/python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'Jarvis Python Library',
+    version = '0.1dev',
+    packages = ['jarvis',],
+    license = 'MIT License'
+    long_description = open('README.txt').read(),
+)
