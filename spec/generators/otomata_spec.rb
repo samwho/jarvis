68bec82a03c43626b2de3e6574fbbaf0e643a2bb
Sam Rose <samwho@lbak.co.uk>
2012-04-13 01:50:15 +0100

    Added tests for the Otomata generator.

--- spec/generators/otomata_spec.rb
+++ spec/generators/otomata_spec.rb
@@ -1,3 +1,88 @@
 describe Jarvis::Generators::Otomata do
-  it 'should respond to poke'
+  before :each do
+    @generator = Jarvis::Generators::Otomata.new
+  end
+
+  after :each do
+    @generator = nil
+  end
+
+  it 'should respond to poke' do
+    @generator.poke 1, 1
+    @generator.grid[1][1].should == [:up]
+
+    @generator.poke 1, 1
+    @generator.grid[1][1].should == [:right]
+
+    @generator.poke 1, 1
+    @generator.grid[1][1].should == [:down]
+
+    @generator.poke 1, 1
+    @generator.grid[1][1].should == [:left]
+
+    @generator.poke 1, 1
+    @generator.grid[1][1].should == []
+  end
+
+  it 'should move up properly' do
+    @generator.poke 1, 1
+    @generator.next
+    @generator.grid[1][0].should == [:up]
+  end
+
+  it 'should move right properly' do
+    @generator.poke 1, 1
+    @generator.poke 1, 1
+    @generator.next
+    @generator.grid[2][1].should == [:right]
+  end
+
+  it 'should move down properly' do
+    @generator.poke 1, 1
+    @generator.poke 1, 1
+    @generator.poke 1, 1
+    @generator.next
+    @generator.grid[1][2].should == [:down]
+  end
+
+  it 'should move left properly' do
+    @generator.poke 1, 1
+    @generator.poke 1, 1
+    @generator.poke 1, 1
+    @generator.poke 1, 1
+    @generator.next
+    @generator.grid[0][1].should == [:left]
+  end
+
+  it 'should change direction when something hits a wall' do
+    @generator.poke 1, 1
+    @generator.next
+    @generator.next
+    @generator.grid[1][1].should == [:down]
+  end
+
+  it 'sould respond properly to collisions' do
+    # This is hard to visualise but what I'm trying to test for is the following
+    # three moves:
+    #
+    # +-+-+-+ +-+-+-+ +-+-+-+
+    # | |V| | | | | | | | | |
+    # +-+-+-+ +-+-+-+ +-+-+-+
+    # |>| | | | |O| | |<| | |
+    # +-+-+-+ +-+-+-+ +-+-+-+
+    # | | | | | | | | | |V| |
+    # +-+-+-+ +-+-+-+ +-+-+-+
+    @generator.poke 1, 0
+    @generator.poke 1, 0
+    @generator.poke 1, 0
+
+    @generator.poke 0, 1
+    @generator.poke 0, 1
+
+    @generator.next
+    @generator.next
+
+    @generator.grid[0][1].should == [:left]
+    @generator.grid[1][2].should == [:down]
+  end
 end
