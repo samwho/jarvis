844661c8011732a9d30c79e3ddb3e43c1a0bbfc2
Sam Rose <samwho@lbak.co.uk>
2012-04-16 19:03:16 +0100

    Tidied up the server support file a little and added some more comments.

--- spec/support/server.rb
+++ spec/support/server.rb
@@ -4,9 +4,12 @@ shared_context 'server' do
   @@jarvis_host = 'localhost'
   @@jarvis_port = 1337
 
+  # Starts up the external jarvis process and waits for it to boot. Then it
+  # initiates a connection to the jarvis process under the @@socket variable.
+  #
+  # Commands can be sent to this jarvis instance with the send_command method.
   def start_jarvis
     @@jarvis_pid = fork do
-      $stdout = File.open('/dev/null', 'w')
       exec "#{Jarvis::ROOTDIR}/bin/jarvis"
     end
 
@@ -14,15 +17,19 @@ shared_context 'server' do
     @@socket = TCPSocket.new @@jarvis_host, @@jarvis_port
   end
 
+  # Sends a command to the jarvis server that's running and returns the string
+  # response from the server.
   def send_command command
     @@socket.print command
     @@last_response = @@socket.recv 4096
   end
 
+  # Refers to the response from the last command sent to jarvis.
   def last_response
     @@last_response
   end
 
+  # Stops the external jarvis process and closes the socket connection to it.
   def stop_jarvis
     if @@jarvis_pid
       @@socket.close if @@socket
@@ -32,10 +39,14 @@ shared_context 'server' do
     end
   end
 
+
+  # RSpec hook to start jarvis before every test case in this context.
   before :each do
+    @last_response = nil
     start_jarvis
   end
 
+  # RSpec hook to stop jarvis after every test case in this context.
   after :each do
     stop_jarvis
   end
