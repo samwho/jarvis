f9cc40db036d845bd0a4e437ad623cd576c64a09
Sam Rose <samwho@lbak.co.uk>
2012-04-23 19:28:47 +0100

    It looks like the port number was indeed the problem. Changing the Python library tests to reflect this.

--- client_libs/python/test/helpers.py
+++ client_libs/python/test/helpers.py
@@ -7,12 +7,12 @@ class ServerHelper(unittest.TestCase):
     DEVNULL = open('/dev/null', 'w')
 
     def setUp(self):
-        self.process = subprocess.Popen(["jarvis", "--testing"],
+        self.process = subprocess.Popen(["jarvis", "--testing", "-p", "12826"],
                 stdout=self.DEVNULL, stderr=self.DEVNULL)
         # Sleep for a second to let the server boot up. Quick and dirty but I
         # can't think of a better way of doing it.
         time.sleep(1)
-        self.jarvis  = Jarvis()
+        self.jarvis  = Jarvis("localhost", 12826)
 
     def tearDown(self):
         self.jarvis.kill_server()
